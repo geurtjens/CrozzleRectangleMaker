8dbd0ecd47ef15069a77e82366e928da29717b93
Michael Geurtjens <geurtjens@me.com>
2023-06-11 06:30:13 +1000

    Extend [ShapeModel] to make filtering less verbose syntax #36

--- Sources/CrozzleRectangleMaker/Calculators/StrategyCalculator.swift
+++ Sources/CrozzleRectangleMaker/Calculators/StrategyCalculator.swift
@@ -90,11 +90,6 @@ public class StrategyCalculator {
             widthMax: widthMax,
             heightMax:heightMax))
         
-        
-        
-        
-        
-        
         let edges = ShapeCalculator.toShapes(edges: EdgeCalculator.Execute(
             words: words,
             scoreMin: 0,
@@ -110,22 +105,22 @@ public class StrategyCalculator {
             heightMax: heightMax)
         
 
-        let hazelnut_merry = ShapeCalculator.filterInclude(shapes: ladder2x2x2,containing: ["HAZELNUT", "MERRY", "HYMN", "AZURE", "JELLY"],from: words)[0]
+        let hazelnut_merry = ladder2x2x2.containing(["HAZELNUT", "MERRY", "HYMN", "AZURE", "JELLY"], from: words)[0]
         print(hazelnut_merry.ToString(words: words))
         
         
         
 
-        let nazareth_sing = ShapeCalculator.filterInclude(shapes: C2x3_2x2,containing: ["NUTS","ZION","EVE","NAZARETH","SING","BELLS"],from: words)[0]
+        let nazareth_sing = C2x3_2x2.containing(["NUTS","ZION","EVE","NAZARETH","SING","BELLS"], from: words)[0]
         print(nazareth_sing.ToString(words: words))
         
-        let toys_tree = ShapeCalculator.filterInclude(shapes: rectangles4x5, containing: ["TOYS","TREE","TOAST","SAUCE"], from: words)[0]
+        let toys_tree = rectangles4x5.containing(["TOYS","TREE","TOAST","SAUCE"], from: words)[0]
         print(toys_tree.ToString(words: words))
         
-        let joy_holly = ShapeCalculator.filterInclude(shapes: edges, containing: ["JOY","HOLLY"], from: words)[1]
+        let joy_holly = edges.containing(["JOY","HOLLY"], from: words)[1]
         print(joy_holly.ToString(words: words))
         
-        let joy_jelly = ShapeCalculator.filterInclude(shapes: edges, containing: ["JOY","JELLY"], from: words)[0]
+        let joy_jelly = edges.containing(["JOY","JELLY"], from: words)[0]
         print(joy_jelly.ToString(words: words))
         
         let joy_holly_jelly = MergeShapesCalculator.Merge_Two_Shapes(smaller: [joy_holly], larger: [joy_jelly], words: words)[0]
@@ -135,12 +130,12 @@ public class StrategyCalculator {
         print(fork.ToString(words: words))
         
         // These are just the edges that are in the winning game
-        let turkey_hymn = ShapeCalculator.filterInclude(shapes: edges, containing: ["TURKEY","HYMN"], from: words)
-        let turkey_sauce = ShapeCalculator.filterInclude(shapes: edges, containing: ["TURKEY","SAUCE"], from: words)
+        let turkey_hymn = edges.containing(["TURKEY","HYMN"], from: words)
+        let turkey_sauce = edges.containing(["TURKEY","SAUCE"], from: words)
         
-        let turkey_pork = ShapeCalculator.filterInclude(shapes: edges, containing: ["TURKEY","PORK"], from: words)
-        let inn_hymn = ShapeCalculator.filterInclude(shapes: edges, containing: ["INN","HYMN"], from: words)
-        let hazelnut_star = ShapeCalculator.filterInclude(shapes: edges, containing: ["HAZELNUT","STAR"], from: words)
+        let turkey_pork = edges.containing(["TURKEY","PORK"], from: words)
+        let inn_hymn = edges.containing(["INN","HYMN"], from: words)
+        let hazelnut_star = edges.containing(["HAZELNUT","STAR"], from: words)
         
         let hazelnut_merry_joy = MergeShapesCalculator.Merge_Two_Shapes(smaller: [joy_holly_jelly], larger: [hazelnut_merry], words: words)
         print(hazelnut_merry_joy[0].ToString(words: words))
@@ -157,7 +152,7 @@ public class StrategyCalculator {
         let toys_turkey_hazelnut_merry_joy_nazareth = MergeShapesCalculator.Merge_Two_Shapes(smaller: [toys_tree], larger: turkey_hazelnut_merry_joy_nazareth, words: words)
         print(toys_turkey_hazelnut_merry_joy_nazareth[0].ToString(words: words))
         
-        let toys_family = ShapeCalculator.filterInclude(shapes: edges, containing: ["TOYS","FAMILY"], from: words)[0]
+        let toys_family = edges.containing(["TOYS","FAMILY"], from: words)[0]
         print(toys_family.ToString(words: words))
         
         let family_toys_turkey_hazelnut_merry_joy_nazareth = MergeShapesCalculator.Merge_Two_Shapes(smaller: [toys_family], larger: toys_turkey_hazelnut_merry_joy_nazareth, words: words)
@@ -256,19 +251,19 @@ public class StrategyCalculator {
         let stock_pay = c2x2[0]
         print(stock_pay.ToString(words: words))
         
-        let stock_debit = ShapeCalculator.filterInclude(shapes: square4x4, containing: ["STOCK","DEBIT","SALE","COST"], from: words)[0]
+        let stock_debit = square4x4.containing(["STOCK","DEBIT","SALE","COST"], from: words)[0]
         print(stock_debit.ToString(words: words))
         
         let stock_pay_debit = MergeShapesCalculator.Merge_Two_Shapes(smaller: [stock_debit], larger: [stock_pay], words: words)[0]
         print(stock_pay_debit.ToString(words: words))
         
-        let stock_short = ShapeCalculator.filterInclude(shapes: square3x3_topRight, containing: ["STOCK","SHORT","COST","BONUS"], from: words)[0]
+        let stock_short = square3x3_topRight.containing(["STOCK","SHORT","COST","BONUS"], from: words)[0]
         print(stock_short.ToString(words:words))
         
         let stock_pay_debit_short = MergeShapesCalculator.Merge_Two_Shapes(smaller: [stock_short], larger: [stock_pay_debit], words: words)[0]
         print(stock_pay_debit_short.ToString(words:words))
         
-        let short_units = ShapeCalculator.filterInclude(shapes: square3x3_topLeft, containing: ["SHORT","BONUS", "UNITS","NET"], from: words)[0]
+        let short_units = square3x3_topLeft.containing(["SHORT","BONUS", "UNITS","NET"], from: words)[0]
         print(short_units.ToString(words: words))
         
         let stock_pay_debit_short_units = MergeShapesCalculator.Merge_Two_Shapes(smaller: [short_units], larger: [stock_pay_debit_short], words: words)[0]
@@ -280,22 +275,22 @@ public class StrategyCalculator {
         let stock_pay_debit_short_units_bull_sold = MergeShapesCalculator.Merge_Two_Shapes(smaller: [units_bull_sold], larger: [stock_pay_debit_short_units], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold.ToString(words: words))
         
-        let bull_notes = ShapeCalculator.filterInclude(shapes: squares3x3, containing: ["BULL","TRUST","SELL", "NOTES"], from: words)[0]
+        let bull_notes = squares3x3.containing(["BULL","TRUST","SELL", "NOTES"], from: words)[0]
         print(bull_notes.ToString(words: words))
         
         let stock_pay_debit_short_units_bull_sold_notes = MergeShapesCalculator.Merge_Two_Shapes(smaller:[bull_notes], larger:[stock_pay_debit_short_units_bull_sold], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes.ToString(words: words))
         
-        let scrip_stock = ShapeCalculator.filterInclude(shapes: square3x3_topRight, containing: ["STOCK","PARITY", "CASH","SCRIP"], from: words)[0]
+        let scrip_stock = square3x3_topRight.containing(["STOCK","PARITY", "CASH","SCRIP"], from: words)[0]
         print(scrip_stock.ToString(words: words))
         
         let stock_pay_debit_short_units_bull_sold_notes_scrip = MergeShapesCalculator.Merge_Two_Shapes(smaller: [scrip_stock], larger: [stock_pay_debit_short_units_bull_sold_notes], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes_scrip.ToString(words: words))
         
-        let capital_scrip = ShapeCalculator.filterInclude(shapes: squares3x3, containing: ["SCRIP","PARITY", "CASH", "CAPITAL"], from: words)[0]
+        let capital_scrip = squares3x3.containing(["SCRIP","PARITY", "CASH", "CAPITAL"], from: words)[0]
         print(capital_scrip.ToString(words: words))
         
-        let capital_scrip2 = ShapeCalculator.filterInclude(shapes: squares3x3, containing: ["SCRIP","PARITY","CAPITAL","TIP"], from: words)[0]
+        let capital_scrip2 = squares3x3.containing(["SCRIP","PARITY","CAPITAL","TIP"], from: words)[0]
         print(capital_scrip2.ToString(words: words))
         
         let capital_scrip_merged = MergeShapesCalculator.Merge_Two_Shapes(smaller: [capital_scrip], larger: [capital_scrip2], words: words)[0]
@@ -304,16 +299,16 @@ public class StrategyCalculator {
         let stock_pay_debit_short_units_bull_sold_notes_scrip_capital = MergeShapesCalculator.Merge_Two_Shapes(smaller: [capital_scrip_merged], larger: [stock_pay_debit_short_units_bull_sold_notes_scrip], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes_scrip_capital.ToString(words: words))
         
-        let short_cut = ShapeCalculator.filterInclude(shapes: square3x3_topRight, containing: ["SHORT","BONUS","COST", "CUT"], from: words)[0]
+        let short_cut = square3x3_topRight.containing(["SHORT","BONUS","COST", "CUT"], from: words)[0]
         print(short_cut.ToString(words: words))
         
         let stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut = MergeShapesCalculator.Merge_Two_Shapes(smaller: [short_cut], larger: [stock_pay_debit_short_units_bull_sold_notes_scrip_capital], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut.ToString(words: words))
         
-        let debit_buy = ShapeCalculator.filterInclude(shapes: edges, containing: ["DEBIT", "BUY"], from: words)[0]
+        let debit_buy = edges.containing(["DEBIT", "BUY"], from: words)[0]
         print(debit_buy.ToString(words: words))
         
-        let buyer_buy = ShapeCalculator.filterInclude(shapes: edges, containing: ["BUY", "BUYER"], from: words)[2]
+        let buyer_buy = edges.containing(["BUY", "BUYER"], from: words)[2]
         print(buyer_buy.ToString(words: words))
         
         let debit_buyer = MergeShapesCalculator.Merge_Two_Shapes(smaller: [debit_buy], larger: [buyer_buy], words: words)[0]
@@ -322,37 +317,37 @@ public class StrategyCalculator {
         let stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut_buyer = MergeShapesCalculator.Merge_Two_Shapes(smaller: [debit_buyer], larger: [stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut_buyer.ToString(words: words))
 
-        let units_iou = ShapeCalculator.filterInclude(shapes: edges, containing: ["UNITS", "IOU"], from: words)[0]
+        let units_iou = edges.containing(["UNITS", "IOU"], from: words)[0]
         print(units_iou.ToString(words: words))
         
         let stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut_buyer_iou = MergeShapesCalculator.Merge_Two_Shapes(smaller: [units_iou], larger: [stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut_buyer], words: words)[0]
         print(stock_pay_debit_short_units_bull_sold_notes_scrip_capital_cut_buyer_iou.ToString(words: words))
         
-        let loss_spot = ShapeCalculator.filterInclude(shapes: edges, containing: ["LOSS", "SPOT"], from: words)[1]
+        let loss_spot = edges.containing(["LOSS", "SPOT"], from: words)[1]
         print(loss_spot.ToString(words: words))
         
-        let spot_tax = ShapeCalculator.filterInclude(shapes: edges, containing: ["SPOT", "TAX"], from: words)[0]
+        let spot_tax = edges.containing(["SPOT", "TAX"], from: words)[0]
         print(spot_tax.ToString(words: words))
         
         let loss_spot_tax = MergeShapesCalculator.Merge_Two_Shapes(smaller: [loss_spot], larger: [spot_tax], words: words)[0]
         print(loss_spot_tax.ToString(words: words))
         
-        let spot_par = ShapeCalculator.filterInclude(shapes: edges, containing: ["SPOT", "PAR"], from: words)[0]
+        let spot_par = edges.containing(["SPOT", "PAR"], from: words)[0]
         print(spot_par.ToString(words: words))
         
-        let par_bear = ShapeCalculator.filterInclude(shapes: edges, containing: ["PAR", "BEAR"], from: words)[1]
+        let par_bear = edges.containing(["PAR", "BEAR"], from: words)[1]
         print(par_bear.ToString(words: words))
         
         let spot_par_bear = MergeShapesCalculator.Merge_Two_Shapes(smaller: [spot_par], larger: [par_bear], words: words)[0]
         print(spot_par_bear.ToString(words: words))
         
-        let bear_bid = ShapeCalculator.filterInclude(shapes: edges, containing: ["BEAR", "BID"], from: words)[0]
+        let bear_bid = edges.containing(["BEAR", "BID"], from: words)[0]
         print(bear_bid.ToString(words: words))
         
         let spot_par_bear_bid = MergeShapesCalculator.Merge_Two_Shapes(smaller: [bear_bid], larger: [spot_par_bear], words: words)[0]
         print(spot_par_bear_bid.ToString(words: words))
         
-        let bid_deed = ShapeCalculator.filterInclude(shapes: edges, containing: ["BID", "DEED"], from: words)[0]
+        let bid_deed = edges.containing(["BID", "DEED"], from: words)[0]
         print(bid_deed.ToString(words: words))
         
         let spot_par_bear_bid_deed = MergeShapesCalculator.Merge_Two_Shapes(smaller: [bid_deed], larger: [spot_par_bear_bid], words: words)[0]
---
+++ Sources/CrozzleRectangleMaker/Extensions/ExtendArrayOfShapeModel.swift
@@ -0,0 +1,15 @@
+//
+//  ExtendArrayOfShapeModel.swift
+//  
+//
+//  Created by Michael Geurtjens on 11/6/2023.
+//
+
+import Foundation
+extension Array where Element == ShapeModel {
+    
+    func containing(_ containing:[String], from: [String]) -> [ShapeModel] {
+        let shapes = ShapeCalculator.filterInclude(shapes: self, containing: containing, from: from)
+        return shapes
+    }
+}
