38bd06690e48b1953a2717fad9fb00e16e1a6541
Michael Geurtjens <geurtjens@me.com>
2023-06-22 07:27:37 +1000

    Try speed up the process by improving MergeCalculator.countOfWordsInShape #66

---
+++ Tests/CrozzleRectangleMakerTests/MergeCalculatorTests.swift
@@ -0,0 +1,37 @@
+//
+//  MergeCalculatorTests.swift
+//  
+//
+//  Created by Michael Geurtjens on 22/6/2023.
+//
+
+import XCTest
+@testable import CrozzleRectangleMaker
+final class MergeCalculatorTests: XCTestCase {
+
+    /// The purpose of this function is to give us how many of each common number there is
+    func test_countOfWordsInShapes() {
+        let matchingShapes = [10,10,20,30,30]
+        let shapeId = 4
+        
+        let result = MergeCalculator.countOfWordsInShapes(sourceShapeId: shapeId, matchingShapes: matchingShapes)
+        XCTAssertEqual(3, result.count)
+        
+        XCTAssertEqual(10, result[0].searchShapeId)
+        XCTAssertEqual(2, result[0].matchingWordCount)
+        XCTAssertEqual(4, result[0].sourceShapeId)
+        
+        XCTAssertEqual(20, result[1].searchShapeId)
+        XCTAssertEqual(1, result[1].matchingWordCount)
+        XCTAssertEqual(4, result[1].sourceShapeId)
+        
+        XCTAssertEqual(30, result[2].searchShapeId)
+        XCTAssertEqual(2, result[2].matchingWordCount)
+        XCTAssertEqual(4, result[2].sourceShapeId)
+        
+        
+        
+        
+    }
+
+}
