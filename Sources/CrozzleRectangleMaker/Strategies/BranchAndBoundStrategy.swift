5a6d97b3a2eb2eaf6d0edb3ad8e50df7603d59a5
Michael Geurtjens <geurtjens@me.com>
2023-09-01 16:11:49 +1000

    Branch and Bound Strategy #109 prototype 3

--- Sources/CrozzleRectangleMaker/Strategies/BranchAndBoundStrategy.swift
+++ Sources/CrozzleRectangleMaker/Strategies/BranchAndBoundStrategy.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 public class BranchAndBoundStrategy {
-    public static func execute(gameId: Int, words: [String]) async -> ShapeModel {
+    public static func execute(gameId: Int, words: [String], repeatTimes: Int) async -> ShapeModel {
         let game = GameList().getGame(gameId: gameId)!
         let wordsInt = WordCalculator.WordsToInt(words: words)
         let searchShapes = getShapes(gameId: gameId, words: words)
@@ -17,32 +17,37 @@ public class BranchAndBoundStrategy {
         let scoresMin = StrategyCalculator.GetScoreMins(gameId: game.gameId)
         
         var sourceShapes = [searchShapes[0]]
-        print(searchShapes[0].ToStringExtended(words: words, gameId: gameId, winningScore: game.winningScore))
+        var bestShape = searchShapes[0]
+        print(bestShape.ToStringExtended(words: words, gameId: gameId, winningScore: game.winningScore))
         let lookahead = 3
         var backtrackCount = 2
-        for _ in 0..<30 {
+        
+        
+        for _ in 0..<repeatTimes {
             for _ in 0..<lookahead {
                 let newShapes = await MergeCalculatorV2.ExecuteDifferentShapesAsync(sourceShapes: sourceShapes, searchShapes: searchShapes, searchWordIndex: wordIndex, sourceMax: sourceShapes.count, searchMax: searchShapes.count, words: words, wordsInt: wordsInt, scoresMin: scoresMin, widthMax: game.maxWidth, heightMax: game.maxHeight)
                 if newShapes.count > 0 {
                     sourceShapes = newShapes
                     ShapeCalculator.SortByScoreThenArea(shapes: &sourceShapes)
+                    if bestShape.score < sourceShapes[0].score {
+                        bestShape = sourceShapes[0]
+                        print(bestShape.ToStringExtended(words: words, gameId: gameId, winningScore: game.winningScore))
+                        print(bestShape.mergeHistory)
+                    }
                 } else {
-                    return sourceShapes[0]
+                    return bestShape
                 }
-                
             }
             
             ShapeCalculator.SortByScoreThenArea(shapes: &sourceShapes)
-            let winningShape = sourceShapes[0]
             
-            let historyToInclude = ShapeCalculator.getMergeHistory(shape: winningShape, count: backtrackCount)
+            
+            let historyToInclude = ShapeCalculator.getMergeHistory(shape: bestShape, count: backtrackCount)
             backtrackCount += 1
             sourceShapes = ShapeCalculator.find(shapes: sourceShapes, mergeHistory: historyToInclude)
             
-            print(winningShape.ToStringExtended(words: words, gameId: gameId, winningScore: game.winningScore))
-            
-            if winningShape.score >= game.winningScore {
-                return winningShape
+            if bestShape.score >= game.winningScore {
+                return bestShape
             }
         }
         return sourceShapes[0]
