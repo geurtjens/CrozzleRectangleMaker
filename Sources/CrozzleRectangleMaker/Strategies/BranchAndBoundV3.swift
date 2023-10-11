f3de6c07514ef17da6068da99552f89d08f75ea4
Michael Geurtjens <geurtjens@me.com>
2023-10-12 06:25:52 +1000

    Optimise beam width for all words

--- Sources/CrozzleRectangleMaker/Strategies/BranchAndBoundV3.swift
+++ Sources/CrozzleRectangleMaker/Strategies/BranchAndBoundV3.swift
@@ -785,6 +785,89 @@ public class BranchAndBoundV3 {
         return result
     }
     
+    public static func optimizeBeamWidthAllWords(
+        gameId: Int,
+        lookaheadDepth: Int,
+        maxDepth: Int,
+        minimumBeamWidth: Int,
+        maximumBeamWidth: Int,
+        rootWidth: Int,
+        useGuidedScores: Bool) async -> Int
+    {
+        
+        var lowerWidth = 0
+        var upperWidth = 0
+        var currentWidth = 0
+
+
+            
+        lowerWidth = minimumBeamWidth
+        upperWidth = maximumBeamWidth
+        
+        let lowerWidthShouldFail = await executeGamesAllWords(
+            games: [gameId],
+            lookaheadDepth: lookaheadDepth,
+            beamWidth: lowerWidth,
+            maxDepth: maxDepth,
+            rootWidth: rootWidth,
+            useGuidedScores: useGuidedScores)
+        
+        if lowerWidthShouldFail.count > 0 {
+            
+            print("game \(gameId) lower width of \(minimumBeamWidth) should not produce a winning game.  Skipping this game.")
+            return -1
+            
+        } else {
+        
+            let upperWidthShouldSucceed = await executeGamesAllWords(
+                games: [gameId],
+                lookaheadDepth: lookaheadDepth,
+                beamWidth: upperWidth,
+                maxDepth: maxDepth,
+                rootWidth: rootWidth,
+                useGuidedScores: useGuidedScores)
+        
+            if upperWidthShouldSucceed.count == 0
+            {
+                print("game \(gameId) upper width of \(maximumBeamWidth) should produce a winning game.  Skipping this game.")
+                return -1
+            } else {
+                
+                while lowerWidth != upperWidth {
+                    
+                    currentWidth = Int((Double(lowerWidth) + Double(upperWidth) + 0.5) / 2.0)
+                    print ("CURRENT WIDTH: \(currentWidth)")
+                    
+                    let winnersForCurrent = await executeGamesAllWords(
+                        games: [gameId],
+                        lookaheadDepth: lookaheadDepth,
+                        beamWidth: lowerWidth,
+                        maxDepth: maxDepth,
+                        rootWidth: rootWidth,
+                        useGuidedScores: useGuidedScores)
+                    
+                    if winnersForCurrent.count == 0 {
+                        if lowerWidth == currentWidth {
+                            lowerWidth += 1
+                            currentWidth = lowerWidth
+                        } else {
+                            lowerWidth = currentWidth
+                        }
+                        
+                    } else {
+                        upperWidth = currentWidth
+                    }
+                }
+                print("FINAL SIZE for : \(gameId) is \(currentWidth)")
+                return currentWidth
+            }
+        }
+    }
+    
+    
+    
+    
+    
     public static func optimizeBeamWidth(
         gameId: Int,
         lookaheadDepth: Int,
